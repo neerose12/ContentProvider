22ef899f47ec8a961b8717127d2e004a6206e7fa
Niroj <niroj.mahrjan@gmail.com>
2018-08-01 13:13:45 +0545

    constant added

---
+++ contentprovider/src/main/java/np/com/nettv/contentprovider/ContentConstant.java
@@ -0,0 +1,11 @@
+package np.com.nettv.contentprovider;
+
+public class ContentConstant {
+
+
+    public static final String FAV = "fav";
+    public static final String RECENT = "recent";
+    public static final String CHANNELID = "channel_id";
+    public static final String CHANNELTYPE = "channel_type";
+
+}
